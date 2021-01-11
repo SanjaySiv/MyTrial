941d0f3e4899d7c6dff084a3ef26575c247fbf02
SanjaySiv <sanjay.ks@pitsolutions.com>
2021-01-11 09:42:24 +0530

    variable and string examples added

---
+++ src/com/example/helloworld/StringExample.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+public class StringExample {
+    public static void main(String[] args){
+        int myInt=7;
+
+        String text="Hello";
+        String blank=" ";
+        String name="Bob";
+        String greeting=text+blank+name;  //string concatenation
+
+        System.out.println(greeting);
+
+        System.out.println("My integer is "+myInt);
+
+        double myDouble=7.5;
+
+        System.out.println("My number is "+myDouble);
+    }
+}
---
+++ src/com/example/helloworld/VariablesExample.java
@@ -0,0 +1,26 @@
+package com.example.helloworld;
+
+public class VariablesExample {
+    public static void main(String[] args){
+        int myNumber=88;
+        short myShort=847;
+        long myLong=9797;
+
+        double myDouble=7.34443;
+        float myFloat=324.3f;
+
+        char myChar='s';
+        boolean myBoolean = false;
+
+        byte myByte=127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
