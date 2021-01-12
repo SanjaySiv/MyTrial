3b8d21a04e555d5f0f4d09aa869a583b51979fe8
SanjaySiv <sanjay.ks@pitsolutions.com>
2021-01-12 15:21:11 +0530

    Car company program(searching bmw)

---
+++ src/com/example/helloworld/ArrayOfCarCompanies.java
@@ -0,0 +1,37 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class ArrayOfCarCompanies {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        Scanner str = new Scanner(System.in);
+        System.out.println("Enter the number of companies :");  //array size
+        int size = num.nextInt();
+
+        String[] carCmp = new String[size];
+        System.out.println("Enter the name of companies :");    //array elements
+        for (int i = 0; i < carCmp.length; i++) {
+            carCmp[i] = str.nextLine();
+        }
+
+        for (int i = 0; i < carCmp.length; i++) {
+            if (carCmp[i].equalsIgnoreCase("bmw")) {  //searching for bmw
+                System.out.println("bmw found");
+                String temp = carCmp[i];
+                for (int j = i; j > 0; j--) {
+                    carCmp[j] = carCmp[j - 1];
+                }
+                carCmp[0] = temp;
+                System.out.println("Altered order");
+                for (String cars : carCmp) {
+                    System.out.println(cars);
+                }
+                break;
+            }
+            else if(i== carCmp.length-1){
+                System.out.println("bmw not found");
+            }
+        }
+    }
+}
