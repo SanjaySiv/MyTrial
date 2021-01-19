f7ad18cf8b9764cb6912a19c4d97f772555a2a98
SanjaySiv <sanjay.ks@pitsolutions.com>
2021-01-19 16:16:49 +0530

    Escape characters

---
+++ src/com/example/helloworld/EscapeCharacters.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+public class EscapeCharacters {
+    public static void main(String[] args){
+        System.out.println("Hi, welcome to \"JAVA\"");  //display ""
+        System.out.println("Hi, welcome to \tJAVA");    //tab space
+        System.out.println("Hi, welcome to \nJAVA");    //new line
+        System.out.println("Hi, welcome to \bJAVA");    //back space
+        System.out.println("Hi, welcome to \fJAVA");    //form feed
+        System.out.println("Hi, welcome to \rJAVA");    //carriage return
+        System.out.println("File location: D\\New folder\\String.java");    //back slash
+    }
+}
