fda129111b2bf1e98cd5afd1c1c2c9752993d87e
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-01-29 17:22:15 +0530

    If demo

---
+++ src/udemy/IfCondition.java
@@ -0,0 +1,17 @@
+package udemy;
+
+public class IfCondition {
+    public static void main(String[] args){
+        int a=27;
+        double b=34.3;
+        if(a<b){
+            System.out.println("a is less than b");
+        }
+        else if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("a is equal to b");
+        }
+    }
+}
