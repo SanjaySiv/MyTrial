632767303249770a949075c3eaee9d97d1fd7fac
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-01-29 14:14:27 +0530

    math operation demo

---
+++ src/udemy/MathOperations.java
@@ -0,0 +1,16 @@
+package udemy;
+
+public class MathOperations {
+    public static void main(String[] args){
+        int var1=10;
+        int var2=34;
+        double var3=23.456;
+        double var4=329.34;
+        String var5="Hello ";
+        String var6="world";
+        System.out.println(var1+var2);
+        System.out.println(var3+var4);
+        System.out.println(var5+var6);
+        System.out.println(var1+var3);
+    }
+}
