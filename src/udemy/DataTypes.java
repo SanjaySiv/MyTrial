22cb269dc73fe60f94501e014446bf06b3aa5585
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-01-29 12:55:44 +0530

    Datatypes

---
+++ src/udemy/DataTypes.java
@@ -0,0 +1,16 @@
+package udemy;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        char var1 = 'a';
+        String var2 = "Hello";
+        boolean var3 = false;
+        int var4 = 345;
+        double var5 = 23.56;
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+}
