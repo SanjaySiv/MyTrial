43b9d8e71a50a9632890b3e9d04a15b7968abd11
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-02-09 17:33:27 +0530

    Passing subclass arraylist to a function

--- src/com/example/helloworld/Generics.java
+++ src/com/example/helloworld/Generics.java
@@ -8,18 +8,26 @@ class Machines{
         return "Machines";
     }
 }
-class Cameras{
-
+class Cameras extends Machines{
+    @Override
+    public String toString() {
+        return "Cameras";
+    }
 }
 public class Generics {
     public static void main(String[] args){
-        ArrayList<Machines>list=new ArrayList<>();
-        list.add(new Machines());
-        list.add(new Machines());
-        showList(list);
+        ArrayList<Machines>list1=new ArrayList<>();
+        list1.add(new Machines());
+        list1.add(new Machines());
+        showList(list1);//superclass list
+
+        ArrayList<Cameras>list2=new ArrayList<>();
+        list2.add(new Cameras());
+        list2.add(new Cameras());
+        showList(list2);//subclass list
     }
-    public static void showList(ArrayList<Machines>list){
-        for(Machines str:list){
+    public static void showList(ArrayList<?>list){
+        for(Object str:list){
             System.out.println(str);
         }
     }
