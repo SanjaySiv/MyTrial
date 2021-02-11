3983d4d279a39d30c23d3524caffb9914c2b37e1
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-02-11 11:14:41 +0530

    ArithmeticException

---
+++ src/com/example/helloworld/ExceptionExample.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import java.util.Scanner;
+
+public class ExceptionExample {
+    public static void main(String[] args){
+        Scanner ob=new Scanner(System.in);
+        int a=ob.nextInt();
+        try{
+            int i=100/a;
+        }catch (ArithmeticException e){
+            System.out.println(e);
+        }
+        System.out.println("Rest of the code");
+        System.out.println("entered value is "+a);
+    }
+}
