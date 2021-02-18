73353ced68260c384d8ec253dc2eaf771265818c
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-02-18 18:26:22 +0530

    Enum outside class altered

--- src/com/example/helloworld/EnumExample2.java
+++ src/com/example/helloworld/EnumExample2.java
@@ -1,11 +1,11 @@
 package com.example.helloworld;
 
-enum Season{WINTER, SUMMER, FALL, SPRING}
+enum Seasons{WINTER, SUMMER, FALL, SPRING}
 public class EnumExample2 {
     public static void main(String[] args){
-        Season s=Season.WINTER;
+        Seasons s= Seasons.WINTER;
         System.out.println(s);
-        for(Season en:Season.values()){
+        for(Seasons en: Seasons.values()){
             System.out.println(en);
         }
     }
