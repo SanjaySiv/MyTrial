a1450f45ef97c8380e040e587add7511b9523d33
sanjay.ks <sanjay.ks@pitsolutiions.com>
2021-02-26 17:50:22 +0530

    Iterable example

--- src/com/example/helloworld/IterableExample.java
+++ src/com/example/helloworld/IterableExample.java
@@ -1,15 +1,10 @@
 package com.example.helloworld;
 
 public class IterableExample {
-    public static void main(String[] args){
-        UrlLibrary urlLibrary=new UrlLibrary();
-<<<<<<< HEAD
-        for(String url:urlLibrary){
-            System.out.println(url);
-=======
-        for(String html:urlLibrary){
-            System.out.println(html.length());
->>>>>>> 53d8977 (Iterable example with url)
+    public static void main(String[] args) {
+        UrlLibrary urlLibrary = new UrlLibrary();
+        for (String html : urlLibrary) {
+                System.out.println(html.length());
+            }
         }
     }
-}
--- src/com/example/helloworld/UrlLibrary.java
+++ src/com/example/helloworld/UrlLibrary.java
@@ -1,66 +1,55 @@
 package com.example.helloworld;
 
-<<<<<<< HEAD
-=======
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
->>>>>>> 53d8977 (Iterable example with url)
+
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class UrlLibrary implements Iterable<String>{
-<<<<<<< HEAD
-    private LinkedList<String>urls=new LinkedList<>();
-    UrlLibrary(){
+public class UrlLibrary implements Iterable<String> {
+    UrlLibrary() {
         urls.add("www.google.com");
         urls.add("www.facebook.com");
         urls.add("www.twitter.com");
         urls.add("www.oracle.com");
     }
-    @Override
-    public Iterator<String> iterator() {
-        return urls.iterator();
-=======
-    private final LinkedList<String>urls=new LinkedList<>();
-    private class UrlIterator implements Iterator<String>{
-        private int index=0;
-        @Override
-        public boolean hasNext() {
-            return index<urls.size();
-        }
+        private final LinkedList<String> urls = new LinkedList<>();
+        private class UrlIterator implements Iterator<String> {
+            private int index = 0;
 
-        @Override
-        public String next() {
-            StringBuilder sb=new StringBuilder();
-            try {
-                URL url=new URL(urls.get(index));
-                BufferedReader br=new BufferedReader(new InputStreamReader(url.openStream()));
-                String line;
-                while((line=br.readLine())!=null){
-                    sb.append(line);
-                    sb.append("\n");
-                }
-                br.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            @Override
+            public boolean hasNext() {
+                return index < urls.size();
             }
-            index++;
-            return sb.toString();
-        }
 
+            @Override
+            public String next() {
+                StringBuilder sb = new StringBuilder();
+                try {
+                    URL url = new URL(urls.get(index));
+                    BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line);
+                        sb.append("\n");
+                    }
+                    br.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                index++;
+                return sb.toString();
+            }
+
+            @Override
+            public void remove() {
+                urls.remove(index);
+            }
+        }
         @Override
-        public void remove() {
-            urls.remove(index);
+        public Iterator<String> iterator () {
+            return new UrlIterator();
         }
     }
-    UrlLibrary(){
-        urls.add("www.caveofprogramming.com");
-        urls.add("www.facebook/caveofprogramming.com");
-    }
-    @Override
-    public Iterator<String> iterator() {
-        return new UrlIterator();
->>>>>>> 53d8977 (Iterable example with url)
-    }
-}
